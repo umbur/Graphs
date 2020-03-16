4e667b9e6cb9149dd71c8e47f9b56613cf651b37
umbur <you@example.com>
2020-03-16 15:54:00 -0600

    initial commit

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -4,7 +4,7 @@ Simple graph implementation
 from util import Stack, Queue  # These may come in handy
 
 class Graph:
-
+    # initial commit
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
